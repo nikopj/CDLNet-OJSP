7cf6a5107ac0cd3fb20ff783dd1725854d6faab4
Nikola Janjusevic <npj226@log-2.hpc.nyu.edu>
2022-05-01 11:19:08 -0400

    dmsc preproc fix

--- job.sh
+++ job.sh
@@ -5,14 +5,14 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=3GB
-#SBATCH --job-name=cGs2030b
+#SBATCH --job-name=JDDa
 #SBATCH --mail-type=END
 #SBATCH --mail-user=npj226@nyu.edu
-#SBATCH --output=slurm/cGDLNet-s2030b.out
-#SBATCH --output=slurm/cGDLNet-s2030b.err
+#SBATCH --output=slurm/JDD_CDLNet-s0120b.out
+#SBATCH --output=slurm/JDD_CDLNet-s0120b.err
 
 module load cuda/11.1.74
 source /scratch/npj226/.ptmri/bin/activate
 cd /scratch/npj226/CDLNet-OJSP
-python train.py trained_nets/cGDLNet-s2030b/args.json
+python train.py trained_nets/JDD_CDLNet-s0120b/args.json
 
